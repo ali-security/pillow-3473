4539afef92e649522b1d29d0a65efb3f1c7ce11f
Tal Samucha <tal.s@sealsecurity.io>
2025-09-12 00:18:03 +0300

    try 7

--- .github/workflows/arch-setup.sh
+++ .github/workflows/arch-setup.sh
@@ -1,4 +1,11 @@
-pacman -Sy --noconfirm --needed base-devel libtiff-devel # freetype2 harfbuzz libjpeg-turbo
+# pacman -Sy --noconfirm --needed base-devel libtiff-devel # freetype2 harfbuzz libjpeg-turbo
+
+pacman -Sy --needed --noconfirm base-devel zlib zlib-devel \
+    libjpeg-turbo libjpeg-turbo-dev libtiff libtiff-dev \
+    libwebp libwebp-dev freetype2 freetype2-dev \
+    lcms2 lcms2-dev libimagequant libimagequant-dev \
+    harfbuzz harfbuzz-dev fribidi fribidi-dev libxcb libxcb-dev \
+    openjpeg2 openjpeg2-dev
 
 wget https://www.python.org/ftp/python/3.9.17/Python-3.9.17.tgz
 tar xzf Python-3.9.17.tgz
