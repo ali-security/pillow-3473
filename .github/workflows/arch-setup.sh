84835789f030718db6c2bc048475bdb9e3ef25f6
Tal Samucha <tal.s@sealsecurity.io>
2025-09-11 23:56:57 +0300

    try 4

--- .github/workflows/arch-setup.sh
+++ .github/workflows/arch-setup.sh
@@ -1,4 +1,2 @@
-pacman -Sy --noconfirm --needed base-devel \
-    zlib libjpeg-turbo libtiff openjpeg2 freetype2 \
-    libwebp lcms2 harfbuzz fribidi xcb-util-image xcb-util-keysyms \
-    libimagequant
+pacman -Sy --noconfirm --needed base-devel freetype2 harfbuzz libjpeg-turbo
+
