22e290183ca88de9e263c3f930b229c80c7a563d
Tal Samucha <tal.s@sealsecurity.io>
2025-09-12 00:00:29 +0300

    try 5

--- .github/workflows/arch-setup.sh
+++ .github/workflows/arch-setup.sh
@@ -1,2 +1,12 @@
 pacman -Sy --noconfirm --needed base-devel freetype2 harfbuzz libjpeg-turbo
 
+wget https://www.python.org/ftp/python/3.9.17/Python-3.9.17.tgz
+tar xzf Python-3.9.17.tgz
+cd Python-3.9.17
+./configure --enable-optimizations
+make install
+cd ..
+
+# Update the symlink in the virtual environment to point to the new Python
+ln -sf /Python-3.9.17/python /vpy3/bin/python3
+
